f13c8893924ecc2ff2e9e248db31c33246582995
Danish Alam <danishalam050@gmail.com>
2025-04-30 00:59:32 +0530

    Updated chat_engine.py

--- backend/chat_engine.py
+++ backend/chat_engine.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-from llama_index.llms.huggingface import HuggingFaceInferenceAPI
+from llama_index.llms.huggingface_api import HuggingFaceInferenceAPI
 
 # Load environment variables
 load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), '.env'))
